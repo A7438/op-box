0abcb2e0eb84f6bc68ce86f4445f7e47e9635767
peak an <64113259+A7438@users.noreply.github.com>
2026-07-02 17:22:35 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -1,88 +1,91 @@
 #!/bin/bash
+set -e
+
 # 修改默认IP
 # sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 # TTYD 免登录
 # sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
-# 移除要替换的包
+
+# 移除要替换的内置包
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/msd_lite
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-netdata
-# Git稀疏克隆，只克隆指定目录到本地
+
+# Git稀疏克隆工具函数
 function git_sparse_clone() {
     branch="$1" repourl="$2" && shift 2
-    git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-    repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-    cd $repodir
-    git sparse-checkout set $@
-    # 将每个子目录移动到 package 下，避免覆盖
-    for dir in $@; do
+    git clone --depth=1 -b "$branch" --single-branch --filter=blob:none --sparse "$repourl"
+    repodir=$(basename "$repourl")
+    cd "$repodir"
+    git sparse-checkout set "$@"
+    for dir in "$@"; do
         [ -d "$dir" ] && mv -f "$dir" ../package/
     done
-    cd .. && rm -rf $repodir
+    cd .. && rm -rf "$repodir"
 }
-# 添加额外插件
+
+# 基础工具插件
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
-# Themes
+
+# 主题
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
 git_sparse_clone main https://github.com/haiibo/packages luci-theme-opentomcat
-# 更改 Argon 主题背景
-cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+
+# Argon 主题自定义背景（文件不存在则跳过）
+[ -f "$GITHUB_WORKSPACE/images/bg1.jpg" ] && cp -f "$GITHUB_WORKSPACE/images/bg1.jpg" package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+
 # 晶晨宝盒
 git_sparse_clone main https://github.com/ophub/luci-app-amlogic luci-app-amlogic
 sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/haiibo/OpenWrt'|g" package/luci-app-amlogic/root/etc/config/amlogic
-# sed -i "s|kernel_path.*|kernel_path 'https://github.com/ophub/kernel'|g" package/luci-app-amlogic/root/etc/config/amlogic
 sed -i "s|ARMv8|ARMv8_MINI|g" package/luci-app-amlogic/root/etc/config/amlogic
-# SmartDNS
+
+# DNS 相关
 git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
-# msd_lite
 git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lite
 git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
-# MosDNS
 git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-# Alist
+
+# 网盘与存储
 git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
-# iStore
+
+# iStore 应用商店
 git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
 git_sparse_clone main https://github.com/linkease/istore luci
-# 在线用户
+
+# 在线用户统计
 git_sparse_clone main https://github.com/haiibo/packages luci-app-onliner
 sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings
 sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
 chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
-# x86 型号只显示 CPU 型号
+
+# 系统显示优化
 sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
-# 修改本地时间格式
 sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
-# 修改版本为编译日期
+
+# 修改版本号为编译日期
 date_version=$(date +"%y.%m.%d")
-orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+orig_version=$(grep DISTRIB_REVISION= package/lean/default-settings/files/zzz-default-settings | awk -F "'" '{print $2}')
 sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-settings/files/zzz-default-settings
-# 修复 hostapd 报错
-cp -f $GITHUB_WORKSPACE/scripts/011-fix-mbo-modules-build.patch package/network/services/hostapd/patches/011-fix-mbo-modules-build.patch
-# 修复 armv8 设备 xfsprogs 报错
+
+# 修复 hostapd 编译报错（补丁不存在则跳过）
+[ -f "$GITHUB_WORKSPACE/scripts/011-fix-mbo-modules-build.patch" ] && cp -f "$GITHUB_WORKSPACE/scripts/011-fix-mbo-modules-build.patch" package/network/services/hostapd/patches/011-fix-mbo-modules-build.patch
+
+# 修复 armv8 设备 xfsprogs 编译报错
 sed -i 's/TARGET_CFLAGS.*/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/g' feeds/packages/utils/xfsprogs/Makefile
-# 修改 Makefile
-# 只修改 luci 应用的 Makefile
+
+# 修正第三方插件 Makefile 路径
 find package/luci-* -name "Makefile" -exec sed -i 's|../../luci.mk|$(TOPDIR)/feeds/luci/luci.mk|g' {} \;
-# 仅对 golang 包修改
 find package/ -path "*/golang/*" -name "Makefile" -exec sed -i 's|../../lang/golang/golang-package.mk|$(TOPDIR)/feeds/packages/lang/golang/golang-package.mk|g' {} \;
-# 取消主题默认设置
-find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
-# 调整 Docker 到 服务 菜单（仅当存在时）
-# 调整 ZeroTier 到 服务 菜单
-# sed -i 's/vpn/services/g; s/VPN/Services/g' feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
-# sed -i 's/vpn/services/g' feeds/luci/applications/luci-app-zerotier/luasrc/view/zerotier/zerotier_status.htm
-# 取消对 samba4 的菜单调整
-# sed -i '/samba4/s/^/#/' package/lean/default-settings/files/zzz-default-settings
-./scripts/feeds update -a
-./scripts/feeds install -a
+
+# 取消主题强制默认设置
+find package/luci-theme-*/* -type f -name '*luci-theme-*' -exec sed -i '/set luci.main.mediaurlbase/d' {} \; 2>/dev/null || true
